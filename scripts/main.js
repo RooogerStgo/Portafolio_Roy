52287bdc38fdae188221127b4c138c93969f9859
DESKTOP-7RFL8NM\Roystgo <rogerstgo117@gmail.com>
2020-07-11 11:59:17 -0500

    nuevas carpetas

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+/*var persona1 ='Roger';
+var sueldo1 = 100;
+var telefono1 = 22258564;
+
+var info = "hola soy " +persona1 + ', mi número de telefono es: ' + telefono1 + ', mi sueldo es de: ' + sueldo1;
+console.log(info);*/
+
+/*function personalInfo(nombre,edad,telefono){
+    var info = "hola soy " +nombre + ', mi número de telefono es: ' + telefono + ', tengo: ' + edad + " años";
+console.log(info);
+
+}
+
+personalInfo("Max",20,335895745);*/
+
+function personalInfo(nombre,edad,telefono){
+    return info = "hola soy " +nombre + ', mi número de telefono es: ' + telefono + ', tengo: ' + edad + " años";}
+
+console.log(personalInfo("Max",20,3658794));
+
+function guardarElementos(id,seccion,tipo,plataforma){
+    return elemento = "Id: "+id + ", Sección: " + seccion + ", Tipo: " + tipo +", Plataforma: "+ plataforma;
+}
+
+console.log(guardarElementos(123,"Prefab","fbx","Blender"));
+
+
+
+
